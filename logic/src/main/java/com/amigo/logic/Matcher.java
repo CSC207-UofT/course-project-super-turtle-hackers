bba2cad550763a76db8ec404d0ff6e94ec8438a4
aksh-n <akshatn77@gmail.com>
2021-10-15 14:12:23 -0400

    Create test directory for logic

---
+++ logic/src/main/java/com/amigo/logic/Matcher.java
@@ -0,0 +1,8 @@
+package com.amigo.logic;
+
+public class Matcher {
+    /* for each user in UserDatabase
+    *
+    *
+     */
+}
---
+++ logic/src/test/java/com/amigo/logic/MatchTest.java
@@ -0,0 +1,8 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+class MatchTest {
+    // Test goes here
+}
