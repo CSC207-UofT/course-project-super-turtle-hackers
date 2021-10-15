f3cab66ec6002b522b4c93051833f8fdb1978182
aksh-n <akshatn77@gmail.com>
2021-10-14 22:10:22 -0400

    Create Course class

---
+++ logic/src/main/java/com/amigo/logic/Course.java
@@ -0,0 +1,25 @@
+package com.amigo.logic;
+
+public class Course {
+    private String courseCode;
+    private String lecture;
+    private String tutorial;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getLecture() {
+        return lecture;
+    }
+
+    public String getTutorial() {
+        return tutorial;
+    }
+
+    public Course(String courseCode, String lecture, String tutorial) {
+        this.courseCode = courseCode;
+        this.lecture = lecture;
+        this.tutorial = tutorial;
+    }
+}
