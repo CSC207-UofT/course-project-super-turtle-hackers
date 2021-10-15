fd61ac478ba5eb783c236938f621608253e09fad
Dien Nguyen <diennguyen190822@gmail.com>
2021-10-15 14:46:01 -0400

    Complete Constructor for User class

--- logic/src/main/java/com/amigo/logic/User.java
+++ logic/src/main/java/com/amigo/logic/User.java
@@ -3,7 +3,10 @@ package com.amigo.logic;
 public class User {
     private Profile profile;
     private String id;
-//    private matchHistory matchHistory;
+//    private matchHistory matchHistory; not used for phase 0
 
-//    public User()
+    public User(Profile profile, String id) {
+        this.profile = profile;
+        this.id = id;
+    }
 }
