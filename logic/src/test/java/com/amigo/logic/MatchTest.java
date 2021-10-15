1838eafe482ca49a471c33a10d26b648acdbbb60
Dien Nguyen <diennguyen190822@gmail.com>
2021-10-15 15:39:38 -0400

    Write one test for Match class

--- logic/src/test/java/com/amigo/logic/MatchTest.java
+++ logic/src/test/java/com/amigo/logic/MatchTest.java
@@ -1,8 +1,40 @@
+package com.amigo.logic;
+
 import org.junit.*;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashSet;
 
-class MatchTest {
-    // Test goes here
+
+public class MatchTest {
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test(timeout = 100)
+    public void testHaveSameCourse() {
+        Course course1 = new Course("CSC207", "LEC0101", "TUT0501");
+        Course course2 = new Course("CSC263", "LEC0201", "TUT0301");
+        Course course3 = new Course("STA247", "LEC0201", "TUT0301");
+        HashSet<Course> profile1courses = new HashSet<Course>(Arrays.asList(course1, course2));
+        HashSet<Course> profile2courses = new HashSet<Course>(Arrays.asList(course2, course3));
+        Profile profile1 = new Profile("Dien", 2, "Computer Science",
+                profile1courses, "dien@mail.utoronto.ca");
+        Profile profile2 = new Profile("Akshat", 2, "Computer Science",
+                profile2courses, "akshat@mail.utoronto.ca");
+        User user1 = new User(profile1, "dien001");
+        User user2 = new User(profile2, "aksh002");
+        Match match1 = new Match(user1, user2, LocalDateTime.now());
+        HashSet<Course> user1courses = new HashSet<>(user1.getProfile().getCourses());
+        HashSet<Course> user2courses = new HashSet<>(user2.getProfile().getCourses());
+        user1courses.retainAll(user2courses);
+        assertEquals(user1courses.size(),1);
+    }
 }
