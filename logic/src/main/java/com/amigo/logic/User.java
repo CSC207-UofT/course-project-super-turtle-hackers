189d2d4f2bc150737a80d55666733d71440af585
aksh-n <akshatn77@gmail.com>
2021-10-16 15:46:31 -0400

    Add currentMatches to User class

--- logic/src/main/java/com/amigo/logic/User.java
+++ logic/src/main/java/com/amigo/logic/User.java
@@ -1,13 +1,19 @@
 package com.amigo.logic;
 
+import java.util.ArrayList;
+
 public class User {
+    // TODO: Add a flag for report
+    // TODO: Add a checkbox for Wildcard Matching
     private Profile profile;
     private String id;
+    private ArrayList<Match> currentMatches;
 //    private matchHistory matchHistory; not used for phase 0
 
     public User(Profile profile, String id) {
         this.profile = profile;
         this.id = id;
+        this.currentMatches = new ArrayList<Match>();
     }
 
     public Profile getProfile() {
@@ -17,6 +23,8 @@ public class User {
     public String getId() {
         return id;
     }
+
+    public ArrayList<Match> getCurrentMatches() {return currentMatches;}
 
     public boolean equals(User user) {
         // If the ID of this user and the user is the same, then  return true
