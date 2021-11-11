d69b259c64c314b53f394a459a439ad1c8188988
Dien Nguyen <diennguyen190822@gmail.com>
2021-11-10 23:43:36 -0500

    Write test cases of getUserById() and getUsers() methods in UserManagerTest.java
    (cherry picked from commit 030f212178f7389d75f31e3db706172aacdc396f)

---
+++ logic/src/test/java/com/amigo/logic/UserManagerTest.java
@@ -0,0 +1,54 @@
+package com.amigo.logic;
+
+import org.junit.*;
+
+import javax.naming.ldap.Control;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.junit.Assert.*;
+
+public class UserManagerTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test (timeout = 100)
+    public void testGetUserById() {
+        UserDatabase db = new UserTextDatabase();
+        UserManager manager = new UserManager(db);
+        List<User> listOfUsers = db.getUsers();
+        String firstUserId = listOfUsers.get(0).getId();
+        String middleUserId = listOfUsers.get((listOfUsers.size() / 2)).getId();
+        String lastUserId = listOfUsers.get((listOfUsers.size()) - 1).getId();
+        User firstUser = manager.getUserById(firstUserId);
+        User middleUser = manager.getUserById(middleUserId);
+        User lastUser = manager.getUserById(lastUserId);
+        assertEquals(firstUser.getProfile().getName(), "Akshat Naik");
+        assertEquals(middleUser.getProfile().getName(), "Tony Hu");
+        assertEquals(lastUser.getProfile().getName(), "Susan Cao");
+    }
+
+    @Test (timeout = 100)
+    public void testGetUsers() {
+        UserDatabase db = new UserTextDatabase();
+        UserManager manager = new UserManager(db);
+        List<User> listOfUsers = db.getUsers();
+        Map<String, User> allUsers = manager.getUsers();
+        for (User user : listOfUsers) {
+            assertEquals(allUsers.size(), 9);
+        }
+        for (User user : allUsers.values()) {
+            boolean hasUser = false;
+            for (User user2 : listOfUsers) {
+                if (Objects.equals(user.getProfile().getName(), user2.getProfile().getName())) {
+                    hasUser = true;
+                }
+                assertTrue(String.valueOf(hasUser), true);
+            }
+        }
+    }
+}
