e50ad3a9e7c6b58ac855c0049d4854740b4369a1
tonyhu-x <807769469hjt@gmail.com>
2021-11-13 14:19:31 -0500

    Add docstrings for ControlSystem

--- app/src/main/java/com/amigo/logic/ControlSystem.java
+++ app/src/main/java/com/amigo/logic/ControlSystem.java
@@ -28,74 +28,102 @@ public class ControlSystem {
             register();
         }
         this.doMatching();
-        ArrayList<User> users =  new ArrayList<User>(this.manager.getUsers().values());
-        for (User user: users) {
+        ArrayList<User> users = new ArrayList<>(this.manager.getUsers().values());
+        for (User user : users) {
             Profile profile = user.getProfile();
             System.out.println(profile.getName() + ": " + user.toStringCurrentMatches());
         }
-
     }
 
+    /**
+     * Modifies user info in memory but does not write into database.
+     */
     private void edit(User user) {
         String response = ui.runEdit();
         if (response.equals("y")) {
             String editItem = ui.chooseEdit();
             if (editItem.equals("1")) {
                 editUserName(user);
-            } else if (editItem.equals("2")) {
+            }
+            else if (editItem.equals("2")) {
                 editProgramOfStudy(user);
-            } else if (editItem.equals("4")) {
+            }
+            else if (editItem.equals("4")) {
                 editContactInfo(user);
-            } else {
+            }
+            else {
                 editCourses(user);
             }
         }
     }
 
+    /**
+     * Adds or removes courses from main.
+     */
     private void editCourses(User user) {
         String addOrRemoveCourse = ui.inputAddOrRemoveCourse();
         if (addOrRemoveCourse.equals("1")) {
             String newCourseCode = ui.inputNewCourse();
             manager.addCourses(user, new Course(newCourseCode));
-        } else {
+        }
+        else {
             String removeCourseCode = ui.inputRemoveCourse();
             manager.removeCourses(user, removeCourseCode);
         }
     }
 
+    /**
+     * Edits user's contact info.
+     */
     private void editContactInfo(User user) {
         String newContactInfo = ui.inputNewContactInfo();
         manager.editContactInfo(user, newContactInfo);
     }
 
+    /**
+     * Edits user's program of study.
+     */
     private void editProgramOfStudy(User user) {
         String newProgramOfStudy = ui.inputNewProgramOfStudy();
         manager.editProgramOfStudy(user, newProgramOfStudy);
     }
 
+    /**
+     * Edits user's username.
+     */
     private void editUserName(User user) {
         String newName = ui.inputNewName();
         manager.editName(user, newName);
     }
 
+    /**
+     * Runs the login functionality.
+     */
     private void login() {
         String id = ui.runLogin();
         User user = manager.getUserById(id);
         // the requested user doesn't exist
         if (user == null) {
             // not doing anything yet
-        } else {
+        }
+        else {
             edit(user);
         }
     }
 
+    /**
+     * Runs the register functionality.
+     */
     private void register() {
         String[] inputs = ui.runRegister();
         ui.runRegisterSuccess(manager.createUser(inputs, true));
     }
 
+    /**
+     * Runs the matching algorithm.
+     */
     public void doMatching() {
-        ArrayList<User> users =  new ArrayList<User>(this.manager.getUsers().values());
+        ArrayList<User> users = new ArrayList<User>(this.manager.getUsers().values());
         Matcher matcher = new Matcher();
         HashMap<String, ArrayList<Match>> matchResults = matcher.matching(users);
         this.manager.modifyCurrentMatches(matchResults);
