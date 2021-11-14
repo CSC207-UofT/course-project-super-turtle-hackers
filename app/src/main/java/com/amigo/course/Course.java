e0606c3d8bb2ff54f0a1476cea69b31eb769e928
tonyhu-x <807769469hjt@gmail.com>
2021-11-14 13:21:19 -0500

    Reformat and add javadoc to Course

--- app/src/main/java/com/amigo/course/Course.java
+++ app/src/main/java/com/amigo/course/Course.java
@@ -1,9 +1,25 @@
 package com.amigo.course;
 
+/**
+ * A course that the user is taking.
+ */
 public class Course {
     private String courseCode;
     private String lecture;
     private String tutorial;
+
+    /**
+     * Creates a course with only the courseCode
+     */
+    public Course(String courseCode) {
+        this(courseCode, "", "");
+    }
+
+    public Course(String courseCode, String lecture, String tutorial) {
+        this.courseCode = courseCode;
+        this.lecture = lecture;
+        this.tutorial = tutorial;
+    }
 
     public String getCourseCode() {
         return courseCode;
@@ -17,16 +33,9 @@ public class Course {
         return tutorial;
     }
 
-    public Course(String courseCode) {
-        this(courseCode, "", "");
-    }
-
-    public Course(String courseCode, String lecture, String tutorial) {
-        this.courseCode = courseCode;
-        this.lecture = lecture;
-        this.tutorial = tutorial;
-    }
-
+    /**
+     * Returns a String representation of the course readable by MySQL.
+     */
     public String toString() {
         return "Course{" + courseCode + ", "
                 + lecture + ", "
