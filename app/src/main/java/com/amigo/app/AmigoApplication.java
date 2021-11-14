a79cb8c88dfed04e43f68bfc1dbdfdc409daa2a2
tonyhu-x <807769469hjt@gmail.com>
2021-11-13 23:02:18 -0500

    Remove unnecessary annotations

--- app/src/main/java/com/amigo/app/AmigoApplication.java
+++ app/src/main/java/com/amigo/app/AmigoApplication.java
@@ -2,10 +2,8 @@ package com.amigo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class AmigoApplication {
 
 	public static void main(String[] args) {
