5bedc206bd6a6d4beab9038f229abdc83b1e3eac
tonyhu-x <807769469hjt@gmail.com>
2021-11-13 23:06:13 -0500

    Add doc for LoginController

--- app/src/main/java/com/amigo/control/LoginController.java
+++ app/src/main/java/com/amigo/control/LoginController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * Controller for login activities.
+ */
 @Controller
 public class LoginController {
 
