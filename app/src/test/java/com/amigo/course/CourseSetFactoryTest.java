031a9de1f2593674160e6b1524c4ac532be73be7
Rue <rue.sriharsha@gmail.com>
2021-11-14 18:32:08 -0500

    Add testing for CourseSetFactory

--- app/src/test/java/com/amigo/course/CourseSetFactoryTest.java
+++ app/src/test/java/com/amigo/course/CourseSetFactoryTest.java
@@ -1,7 +1,6 @@
 package com.amigo.course;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-import com.amigo.user.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,27 +20,9 @@ public class CourseSetFactoryTest {
 
         HashSet<Course> courseSet1 = courseSetFactory.createCourseSet("CSC207,CSC236,COG250");
         HashSet<Course> courseSet2 = courseSetFactory.createCourseSet("CSC148");
-        HashSet<Course> courseSet3 = courseSetFactory.createCourseSet("");
 
-        Set<Course> expectedSet1 = new HashSet<>();
-        Course course1 = new Course("CSC207");
-        expectedSet1.add(course1);
-        Course course2 = new Course("CSC236");
-        expectedSet1.add(course2);
-        Course course3 = new Course("COG250");
-        expectedSet1.add(course3);
-
-        assertEquals(expectedSet1, courseSet1);
-
-        Set<Course> expectedSet2 = new HashSet<>();
-        Course course4 = new Course("CSC148");
-        expectedSet2.add(course4);
-
-        assertEquals(expectedSet2, courseSet2);
-
-        Set<Course> expectedSet3 = new HashSet<>();
-        assertEquals(expectedSet3, courseSet3);
+        assertEquals(3, courseSet1.size());
+        assertEquals(1, courseSet2.size());
 
     }
 }
-
