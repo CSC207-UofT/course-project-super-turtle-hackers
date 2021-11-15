70b5b093e848f97943c587c96924ea651d45ca95
Dien Nguyen <diennguyen190822@gmail.com>
2021-11-15 14:29:47 -0500

    Implement Factory design pattern by linking with UserFactory, ProfileFactory and CourseSetFactory.

--- app/src/main/java/com/amigo/user/UserManager.java
+++ app/src/main/java/com/amigo/user/UserManager.java
@@ -2,15 +2,22 @@ package com.amigo.user;
 
 import java.util.*;
 import com.amigo.course.Course;
+import com.amigo.course.CourseSetFactory;
 import com.amigo.match.Match;
 
 public class UserManager {
 
     private Map<String, User> users;
-    private UserDatabase database;
+    private final UserDatabase database;
+    private final UserFactory userFactory;
+    private final ProfileFactory profileFactory;
+    private final CourseSetFactory courseSetFactory;
 
     public UserManager(UserDatabase database) {
         this.database = database;
+        this.userFactory = new UserFactory();
+        this.profileFactory = new ProfileFactory();
+        this.courseSetFactory = new CourseSetFactory();
         List<User> userList = database.getUsers();
         users = new HashMap<>(userList.size());
         for (var each : userList) {
@@ -37,15 +44,15 @@ public class UserManager {
         String name = userInfo[0].strip();
         int year = Integer.parseInt(userInfo[1]);
         String prog = userInfo[2].strip();
-        HashSet<Course> courses = createCourseSet(userInfo[3]);
+        HashSet<Course> courses = courseSetFactory.createCourseSet(userInfo[3]);
         String contact = userInfo[4];
-        Profile p = new Profile(name, year, prog, courses, contact);
-        String id = name.split(" ")[0].toLowerCase() + (int) (1000 * Math.random() + 1);
-        User user = new User(p, id);
+        Profile p = profileFactory.createProfile(name, year, prog, courses, contact);
+//        String id = name.split(" ")[0].toLowerCase() + (int) (1000 * Math.random() + 1);
+        User user = userFactory.createUser(p);
         // TODO: not yet doing any checks of duplicates
-        users.put(id, user);
+        users.put(user.getId(), user);
         database.addUser(user);
-        return id;
+        return user.getId();
     }
 
     public void editName (User user, String newName) {
@@ -76,25 +83,6 @@ public class UserManager {
     public void editContactInfo (User user, String newContactInfo) {
         Profile userProfile = user.getProfile();
         userProfile.setContactInfo(newContactInfo);
-    }
-
-    /**
-     * Creates a course set from input source
-     *
-     * THIS IS A DUPLICATE METHOD
-     *
-     * @param source
-     * @return
-     */
-    private HashSet<Course> createCourseSet(String source) {
-        String[] courseStrings = source.split(",");
-        HashSet<Course> courseSet = new HashSet<Course>();
-        for (var str : courseStrings) {
-            Course c = new Course(str.strip());
-            courseSet.add(c);
-        }
-
-        return courseSet;
     }
 
     /**
