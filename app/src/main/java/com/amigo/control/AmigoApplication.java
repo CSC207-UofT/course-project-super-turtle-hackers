6a28556105f08361d19b354cfd52ee3cc570e138
aksh-n <akshatn77@gmail.com>
2021-11-14 20:29:07 -0500

    Resolve conflicts

--- app/src/main/java/com/amigo/control/AmigoApplication.java
+++ app/src/main/java/com/amigo/control/AmigoApplication.java
@@ -2,8 +2,12 @@ package com.amigo.control;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.amigo.control", "com.amigo.course"})
+@EntityScan(basePackages = {"com.amigo.course"})
+@EnableJpaRepositories("com.amigo.course")
 public class AmigoApplication {
 
 	public static void main(String[] args) {
--- app/src/main/java/com/amigo/control/DatabaseController.java
+++ app/src/main/java/com/amigo/control/DatabaseController.java
@@ -7,6 +7,7 @@ import com.amigo.course.CourseRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,7 +37,7 @@ public class DatabaseController {
     /**
      * Returns all the courses saved in the database
      */
-    @PostMapping(path="/all")
+    @GetMapping(path="/all")
     public @ResponseBody List<Course> getAllCourses() {
         return courseRepository.findAll();
     }
