325bb95458f804ad8943f1379a4536092f67fe15
tonyhu-x <807769469hjt@gmail.com>
2021-11-25 23:28:32 -0500

    Add configuration properties annotation

--- app/src/main/java/com/amigo/control/AmigoApplication.java
+++ app/src/main/java/com/amigo/control/AmigoApplication.java
@@ -3,14 +3,16 @@ package com.amigo.control;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.amigo.control", "com.amigo.course"})
 @EntityScan(basePackages = {"com.amigo.course"})
 @EnableJpaRepositories(basePackages =  {"com.amigo.course"})
 @ComponentScan("com.amigo") // enabling component scan for same-level packages (by indicating closest common
                             // parent package)
+@ConfigurationPropertiesScan("com.amigo.config")
 public class AmigoApplication {
 
 	public static void main(String[] args) {
