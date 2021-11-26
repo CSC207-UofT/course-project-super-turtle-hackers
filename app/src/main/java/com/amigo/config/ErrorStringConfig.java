a0672ff284f8fa4ac15523237f2e6a336426cbef
tonyhu-x <807769469hjt@gmail.com>
2021-11-25 23:35:19 -0500

    Add Javadoc

--- app/src/main/java/com/amigo/config/ErrorStringConfig.java
+++ app/src/main/java/com/amigo/config/ErrorStringConfig.java
@@ -3,6 +3,9 @@ package com.amigo.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+/**
+ * A POJO class that holds String constants for error messages.
+ */
 @ConfigurationProperties(prefix = "error")
 @ConstructorBinding     // to make the object immutable
 public class ErrorStringConfig {
