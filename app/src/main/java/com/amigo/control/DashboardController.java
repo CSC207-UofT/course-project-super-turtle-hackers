d5245580741ffef0d6c45d45ef3eac4b09724c73
Tony (Juntao) Hu <807769469hjt@gmail.com>
2021-12-05 17:07:34 -0500

    Connect matching screen to code

--- app/src/main/java/com/amigo/control/DashboardController.java
+++ app/src/main/java/com/amigo/control/DashboardController.java
@@ -5,6 +5,7 @@ import com.amigo.user.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,6 +26,26 @@ public class DashboardController {
         }
         ModelAndView mav = new ModelAndView("dashboard");
         presenter.populate(mav.getModel());
+        return mav;
+    }
+
+    @GetMapping("/matching-screen")
+    public ModelAndView showMatchingScreen() {
+        ModelAndView mav = new ModelAndView("matching-screen");
+        User currentUser = presenter.getUser();
+
+        int counter = 0;
+        for (var each : currentUser.getCurrentMatches()) {
+            UserPresenter p = new UserPresenter();
+            if (each.getUser1() == currentUser){
+                p.setUser(each.getUser2());
+            }
+            else {
+                p.setUser(each.getUser1());
+            }
+            p.setSuffix(String.valueOf(++counter));
+            p.populate(mav.getModel());
+        }
         return mav;
     }
 }
