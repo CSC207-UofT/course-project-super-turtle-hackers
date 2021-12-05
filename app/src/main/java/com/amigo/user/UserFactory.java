820ba9cc96b80e169ef8600770da702d7fc8e21c
Tony (Juntao) Hu <807769469hjt@gmail.com>
2021-12-05 18:20:24 -0500

    Remove deprecated flag

--- app/src/main/java/com/amigo/user/UserFactory.java
+++ app/src/main/java/com/amigo/user/UserFactory.java
@@ -3,7 +3,6 @@ package com.amigo.user;
 /**
  * Factory that curates form input and creates a {@code User} object.
  */
-@Deprecated
 public class UserFactory {
 
     /**
@@ -11,7 +10,6 @@ public class UserFactory {
      * @param profile a Profile object that contains all the information of the user
      * @return a new User object
      */
-    @Deprecated
     public User createUser(Profile profile) {
         String name = profile.getName();
         String id = name.split(" ")[0].toLowerCase() + (int) (1000 * Math.random() + 1);
